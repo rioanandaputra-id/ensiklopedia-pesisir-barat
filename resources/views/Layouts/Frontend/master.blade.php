33e28c746655a1218b47f28c741f790e600471aa
rioanandaputra-id <rioanandaputra1998@gmail.com>
2022-01-26 10:55:16 +0700

    create htaccess and update .env.example

--- resources/views/Layouts/Frontend/master.blade.php
+++ resources/views/Layouts/Frontend/master.blade.php
@@ -7,10 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <link href="{{ asset('assets/css/core.style.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('assets/css/colorbox.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css">
-    <link type="text/css" rel="stylesheet" media="all" href="{{ asset('assets/css/skin.css') }}">
+    <link href="{{ asset('assets/Frontend/css/core.style.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/Frontend/css/colorbox.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/Frontend/css/style.css') }}" rel="stylesheet" type="text/css">
+    <link type="text/css" rel="stylesheet" media="all" href="{{ asset('assets/Frontend/css/skin.css') }}">
     <style>
         .bg-white {
             background-color: #fff;
@@ -53,27 +53,27 @@
     @include('Layouts.Frontend.footer')
     @show
 
-    <script type="text/javascript" src="{{ asset('assets/js/jquery.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/form.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/gui.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/jquery.colorbox-min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/jquery.jcarousel.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/supersized.3.2.7.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/Frontend/js/jquery.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/Frontend/js/form.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/Frontend/js/gui.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/Frontend/js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/Frontend/js/jquery.colorbox-min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/Frontend/js/jquery.jcarousel.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/Frontend/js/supersized.3.2.7.min.js') }}"></script>
     <script type="text/javascript">
         jQuery(function ($) {
             $.supersized({
                 slides: [{
-                    image: "{{ asset('assets/img/1.jpg') }}"
+                    image: "{{ asset('assets/Frontend/img/1.jpg') }}"
                 },
                 {
-                    image: "{{ asset('assets/img/2.jpg') }}"
+                    image: "{{ asset('assets/Frontend/img/2.jpg') }}"
                 },
                 {
-                    image: "{{ asset('assets/img/3.jpg') }}"
+                    image: "{{ asset('assets/Frontend/img/3.jpg') }}"
                 },
                 {
-                    image: "{{ asset('assets/img/4.jpg') }}"
+                    image: "{{ asset('assets/Frontend/img/4.jpg') }}"
                 },
                 ]
             });
